bae66ddb657a2925c9b30d8723582a748afc6b2c
Grzegorz <gprzytula95@gmail.com>
2018-05-23 17:29:01 +0200

    Add pcap file feature

--- bcyb-sniffer.go
+++ bcyb-sniffer.go
@@ -4,20 +4,24 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcapgo"
 )
 
 var (
-	device       string = ""
-	snapshot_len int32  = 1024
-	promiscuous  bool   = false
-	err          error
-	timeout      time.Duration = 30 * time.Second
-	handle       *pcap.Handle
-
+	device           string = ""
+	snapshot_len     int32  = 1024
+	promiscuous      bool   = false
+	err              error
+	timeout          time.Duration = 30 * time.Second
+	handle           *pcap.Handle
+	snapshotLen      uint32 = 1024
+	file_writer      *pcapgo.Writer
 	should_save_pcap bool = false
 )
 
@@ -29,6 +33,13 @@ func init() {
 
 func main() {
 	flag.Parse()
+
+	if should_save_pcap {
+		f, _ := os.Create("captured.pcap")
+		file_writer = pcapgo.NewWriter(f)
+		file_writer.WriteFileHeader(snapshotLen, layers.LinkTypeEthernet)
+		defer f.Close()
+	}
 	// Open device
 	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
 	if err != nil {
@@ -41,5 +52,8 @@ func main() {
 	for packet := range packetSource.Packets() {
 		// Process packet here
 		fmt.Println(packet)
+		if should_save_pcap {
+			file_writer.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
+		}
 	}
 }
